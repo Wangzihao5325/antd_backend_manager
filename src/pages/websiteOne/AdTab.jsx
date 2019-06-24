82932143a6fc2dc4f174cc3cd4e74d7a8b283ada
Wangzihao5325 <416990273@qq.com>
2019-06-24 10:59:59 +0700

    1.增加广告tab

---
+++ src/pages/websiteOne/AdTab.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class AdTab extends Component {
+    render() {
+        return (
+            <p>this is ad tab</p>
+        );
+    }
+}
--- src/pages/websiteOne/index.jsx
+++ src/pages/websiteOne/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Tabs } from 'antd';
 import MainTab from './MainTab';
+import AdTab from './AdTab';
 import ModuleTab from './ModuleTab';
 
 const { TabPane } = Tabs;
@@ -12,7 +13,8 @@ class Website extends Component {
         this.newTabIndex = 0;
         const panes = [
             { title: '全局配置', content: <MainTab />, key: '1', closable: false },
-            { title: '模块1', content: <ModuleTab />, key: '2' },
+            { title: '广告配置', content: <AdTab />, key: '2', closable: false },
+            { title: '模块1', content: <ModuleTab />, key: '3' },
         ];
         this.state = {
             activeKey: panes[0].key,
